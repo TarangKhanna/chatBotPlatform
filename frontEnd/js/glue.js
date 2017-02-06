55ae4cded35c6027ca1b8031be48cbe0517befe9
Joel Van Auken <posiden104@gmail.com>
2017-02-06 17:45:57 -0500

    pulling signin information

---
+++ frontEnd/js/glue.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+  //alert("here");
+})
+
+function login(){
+  var uname = $('#loginUsername')[0].vaue;
+  var pswd = $('#loginPassword')[0].value;
+  console.log(info);
+}
+
+function guestSignin(){
+  var info = $('#guestUsername')[0].value;
+  console.log(info);
+}
+
+function forgotPswd(){
+
+}
+
+function createAcct(){
+  
+}
