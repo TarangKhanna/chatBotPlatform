50f0e1698c857fcfdc25bdf9522e1864db207525
Joel Van Auken <posiden104@gmail.com>
2017-02-10 17:57:00 -0500

    grammar

--- frontEnd/js/glue.js
+++ frontEnd/js/glue.js
@@ -1,42 +1,44 @@
-// $(document).ready(function(){
-//   //alert("here");
+// $(document).ready(function() {
+//
 // })
 
-function login(){
-  var uname = $('#loginUsername')[0].value;
-  var pswd = $('#loginPassword')[0].value;
 
-  // do login things here
 
-  window.location.href = "chathub.html";
+function login() {
+    var uname = $('#loginUsername')[0].value;
+    var pswd = $('#loginPassword')[0].value;
+
+    // do login things here
+
+    window.location.href = "chathub.html";
 }
 
-function guestSignin(){
-  var info = $('#guestUsername')[0].value;
+function guestSignin() {
+    var info = $('#guestUsername')[0].value;
 
-  // do sign in things here
+    // do sign in things here
 
-  window.location.href = "chathub.html";
+    window.location.href = "chathub.html";
 }
 
-function signUp(){
-  var uname = $("#username")[0].value;
-  var email = $("#email")[0].value;
-  var pswd = $("#pswd")[0].value;
+function signUp() {
+    var uname = $("#username")[0].value;
+    var email = $("#email")[0].value;
+    var pswd = $("#pswd")[0].value;
 
-  // do signup things
+    // do signup things
 
-  window.location.href = "chathub.html";
+    window.location.href = "chathub.html";
 }
 
-function forgotPswd(){
-window.location.href = "forgot.html";
+function forgotPswd() {
+    window.location.href = "forgot.html";
 }
 
-function recover(){
-  var email = $("#email")[0].value;
+function recover() {
+    var email = $("#email")[0].value;
 }
 
-function createAcct(){
-  window.location.href = "account.html";
+function createAcct() {
+    window.location.href = "account.html";
 }
