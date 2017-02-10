855d71b2aebed264caec119a065656db490cfdf9
wlarkey <wlarkey@purdue.edu>
2017-02-10 17:10:39 -0500

    front end stuff

--- frontEnd/js/generate_hub.js
+++ frontEnd/js/generate_hub.js
@@ -1,10 +1,22 @@
 function loadcontent() {
 
-  var opening = document.getElementById('headbar');
-  var side = document.getElementById('sidebar');
+  var opening = document.getElementById('roombar');
+  var side = document.getElementById('userbar');
   var main = document.getElementById('mainpage');
-  var content = '';
+  var ucontent = 'userbar<br \>User 1 - in chatroom<br \>User 2 - in chatroom<br \>User 3 - in chatroom<br \>User 1 - in chatroom<br \>User 2 - in chatroom<br \>User 3 - in chatroom<br \>User 1 - in chatroom<br \>User 2 - in chatroom<br \>User 3 - in chatroom<br \>';
+  var rcontent = 'roombar<br \>Room 1 - not empty -<button type="button" class="btn">Enter</button><br \>Room 2 - empty -<button type="button" class="btn">Enter</button><br \>Room 3 - in chatroom<br \>';
+  var mcontent = 'Mainpage';
 
-  
+
+  userbar.innerHTML = ucontent;
+  roombar.innerHTML = rcontent;
+  mainpage.innerHTML = mcontent;
+
+  // RESET COUNTERS
+  content = '';
+  counter = 0;
+
+
+
 
 }
