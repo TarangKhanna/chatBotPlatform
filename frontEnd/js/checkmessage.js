090ab35ec97b5c80cad9946d533359b2bc3359af
Wyatt Larkey <wlarkey@purdue.edu>
2017-02-09 22:16:35 -0500

    check message

--- frontEnd/js/checkmessage.js
+++ frontEnd/js/checkmessage.js
@@ -1,7 +1,18 @@
 function check(string) {
 
   // check validity of message before POST
+  var string = "Test String";
+  var len = string.length;
 
+  if (len < 1) {
+    // send error to user
+  }
+  else if (len > 1024) {
+    // send error to user
+  }
+  else {
+    // POST message here
+  }
 
 
 }
