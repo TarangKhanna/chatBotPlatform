e19a5339d48f0567776fe38ef3dd5fdf74ce33f0
Joel Van Auken <posiden104@gmail.com>
2017-02-17 16:27:06 -0500

    test

--- server.js
+++ server.js
@@ -77,12 +77,9 @@ function getRooms(str, rooms)
 //////////////////////////////////////////////////////////////////////////////////
 
 app.set('views', __dirname + '/public');//view = front end
-<<<<<<< HEAD
-//app.set('view engine', 'jade');
-=======
+
 app.set('view engine', 'jade');
->>>>>>> origin/master
-//app.set('view engine', 'html');
+
 app.set("view options", { layout: false });
 
 app.use(express.static(__dirname + '/public'));//Express helps serve files
